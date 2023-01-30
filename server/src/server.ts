e5010a6169dbbffa1bdcd4ccbba990a6999920ee
JacquelineCasali <jcasalia@yahoo.com.br>
2023-01-30 16:01:04 -0300

    criando tabela

---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+// typeScript - traz a tipagem estática 
+
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+// Método HTTP: get, Post , Put, Patch,Delete
+// get busca informação  (traz lista)
+// post cria alguma coisa,
+// put atualiza  alguma recurso por completo
+// patch atualiza  alguma recurso especifico
+// delete deleta algum recurso
+
+app.register (cors)
+
+// rota
+app.get("/", async()=>{
+const habits = await prisma.habit.findMany()
+   
+
+
+    return habits
+})
+
+app.listen ({
+    port: 3333,
+}).then(()=>{
+console.log("Http Serve running http://localhost:3333")
+})
