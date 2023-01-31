743caf7d0ee1e444d543600cc2629cfc41542f0c
JacquelineCasali <jcasalia@yahoo.com.br>
2023-01-31 13:50:23 -0300

    banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,30 @@
-// typeScript - traz a tipagem estática 
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+const app = fastify();
+const prisma = new PrismaClient();
 
-const app = Fastify()
-const prisma = new PrismaClient()
-// Método HTTP: get, Post , Put, Patch,Delete
-// get busca informação  (traz lista)
-// post cria alguma coisa,
-// put atualiza  alguma recurso por completo
-// patch atualiza  alguma recurso especifico
-// delete deleta algum recurso
+// // Método HTTP: get, Post , Put, Patch,Delete
+// // get busca informação  (traz lista)
+// // post cria alguma coisa,
+// // put atualiza  alguma recurso por completo
+// // patch atualiza  alguma recurso especifico
+// // delete deleta algum recurso
 
-app.register (cors)
+app.register(cors);
 
-// rota
-app.get("/", async()=>{
-const habits = await prisma.habit.findMany()
-   
+// // rota
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany({});
 
+  return habits;
+});
 
-    return habits
-})
-
-app.listen ({
+app
+  .listen({
     port: 3333,
-}).then(()=>{
-console.log("Http Serve running http://localhost:3333")
-})
+  })
+  .then(() => {
+    console.log("Http Serve running http://localhost:3333");
+  });
